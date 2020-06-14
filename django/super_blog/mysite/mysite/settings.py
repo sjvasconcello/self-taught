87cd4b045a43dc3e116c5fd997598c96c03091be
sjvasconcello <sjvasconcello@gmail.com>
2020-06-14 19:53:42 -0400

    Django 3 by Example - pag 45

--- django/super_blog/mysite/mysite/settings.py
+++ django/super_blog/mysite/mysite/settings.py
@@ -114,6 +114,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
