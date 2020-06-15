f99a4e7a9043ad58fc350c67df27071326afd2c2
sjvasconcello <sjvasconcello@gmail.com>
2020-06-15 17:58:59 -0400

    Django 3 by Example - pag 58

--- django/super_blog/mysite/blog/views.py
+++ django/super_blog/mysite/blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 def post_list(request):
@@ -28,16 +28,38 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == "POST":
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comments
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   "comments": comments,
+                   "new_comment": new_comment,
+                   "comment_form": comment_form})
 
 
 def post_share(request, post_id):
     # Retrive post bby id
     post = get_object_or_404(Post, id=post_id, status="published")
     sent = False
-    if request.method ==  "POST":
+    if request.method == "POST":
         # Form was sumitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
