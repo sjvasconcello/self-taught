e47ecae1166976c60baeeeef6f1c7ed443cca7ad
sjvasconcello <sjvasconcello@gmail.com>
2020-06-17 14:44:04 -0400

    Django 3 by Example - pag 67

--- django/super_blog/mysite/blog/views.py
+++ django/super_blog/mysite/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.db.models import Count
 from taggit.models import Tag
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
@@ -38,6 +39,11 @@ def post_detail(request, year, month, day, post):
 
     # List of active comments for this post
     comments = post.comments.filter(active=True)
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count(
+        "tags")).order_by("-same_tags", "-publish")[:4]
 
     new_comment = None
 
@@ -59,7 +65,8 @@ def post_detail(request, year, month, day, post):
                   {'post': post,
                    "comments": comments,
                    "new_comment": new_comment,
-                   "comment_form": comment_form})
+                   "comment_form": comment_form, 
+                   "similar_posts": similar_posts})
 
 
 def post_share(request, post_id):
