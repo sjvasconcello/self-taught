3affc850bb96ac23d49930c89c37927038ccef27
sjvasconcello <sjvasconcello@gmail.com>
2020-06-18 21:13:54 -0400

    Django 3 by Example - pag 73

--- django/super_blog/mysite/blog/templatetags/blog_tags.py
+++ django/super_blog/mysite/blog/templatetags/blog_tags.py
@@ -1,7 +1,9 @@
 from django import template
+from django.db.models import Count
 from ..models import Post
 
 register = template.Library()
+
 
 @register.simple_tag
 def total_posts():
@@ -12,3 +14,8 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by("-publish")[:count]
     return {"latest_post": latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(total_comments=Count("comments")).order_by("-total_comments")[:count]
